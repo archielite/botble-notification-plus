02d6c67fd1c0fe7d58f46b388a520e2d6fb88991
Dinh Quoc Han <dqh@dinhquochan.com>
2023-04-17 18:00:30 +0700

    fix: permissions

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use ArchiElite\NotificationPlus\Http\Controllers\SettingController;
 
 Route::prefix(BaseHelper::getAdminPrefix())->middleware(['web', 'core', 'auth'])->group(function () {
     Route::prefix('notification-plus')->name('notification-plus.')->group(function () {
-        Route::get('settings', [SettingController::class, 'index'])->name('settings');
-        Route::post('settings', [SettingController::class, 'save']);
-        Route::post('send-test-message', [SettingController::class, 'sendTestMessage'])->name('send-test-message');
-        Route::post('get-telegram-chat-ids', [SettingController::class, 'getTelegramChatIds'])->name('get-telegram-chat-ids');
+        Route::group(['permission' => 'notification-plus.settings'], function () {
+            Route::get('settings', [SettingController::class, 'index'])->name('settings');
+            Route::post('settings', [SettingController::class, 'save']);
+            Route::post('send-test-message', [SettingController::class, 'sendTestMessage'])->name('send-test-message');
+            Route::post('get-telegram-chat-ids', [SettingController::class, 'getTelegramChatIds'])->name('get-telegram-chat-ids');
+        });
     });
 });
